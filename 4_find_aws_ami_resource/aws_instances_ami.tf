c6fd2ea5fa037761746f7d2ab195a805391de448
isushnik <isushnik@mail.ru>
2021-10-30 15:40:25 +0300

    Selecting files in the project

--- 4_find_aws_ami_resource/aws_instances_ami.tf
+++ 4_find_aws_ami_resource/aws_instances_ami.tf
@@ -15,11 +15,10 @@ data "aws_ami" "latest_amazon_linux" {
 resource "aws_instance" "serv_amazon_linux" {
   #ami = "ami-02e136e904f3da870" # us-east-1
   #ami = "ami-074cce78125f09d61" # us-east-2
-  ami           = data.aws_ami.latest_amazon_linux.id
-  count         = var.count_server
-  instance_type = "t2.micro"
-  tags          = merge(var.common_tags, { Name = "${var.common_tags["Environment"]} Server_amazon_linux" }) # Development Server_amazon_linux
-
+  ami                    = data.aws_ami.latest_amazon_linux.id
+  count                  = var.count_server
+  instance_type          = "t2.micro"
+  tags                   = merge(var.common_tags, { Name = "${var.common_tags["Environment"]} Server_amazon_linux" }) # Development Server_amazon_linux
   vpc_security_group_ids = [aws_security_group.SG_amazon_linux.id]
 
   lifecycle {
